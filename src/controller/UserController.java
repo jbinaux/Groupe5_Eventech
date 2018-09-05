0ed52c83efb1fd76e139993b30b995d3dcb8ead3
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-09-05 09:51:03 +0200

    create insertUser method in UserController, update gitignore

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,10 +1,42 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import model.User;
+import password.Password;
 
 public class UserController {
 
 	User model;
+	
+	public int InsertUser()
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			Password password = new Password();
+			String url = "jdbc:mysql://localhost:3306/eventech_db";
+			String user = "root";
+			String pwd = password.getPassword();
+
+			Connection con = DriverManager.getConnection(url, user, pwd);
+
+			PreparedStatement ps = con.prepareStatement("insert into users(`nom`,`prenom`,`mail`,`mot_de_passe`"
+					+ ",`domaine_activite`) values(?,?,?,?,?)");
+			ps.setString(1, model.getNom());
+			ps.setString(2, model.getPrenom());
+			ps.setString(3, model.getMail());
+			ps.setString(4, model.getMotDePasse());
+			ps.setString(5, model.getDomaineActivite());
+
+			int s = ps.executeUpdate();
+			return s;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
 	
 	public int getUserId() {
 		return model.getIdUser();
